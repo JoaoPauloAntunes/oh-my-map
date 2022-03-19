8aa9a4238746f480ddd4b9828741896b8430c83c
= <=>
2022-03-19 17:42:47 -0300

    refactor

--- src/components/examples/_maps/_core/ZoomControl/index.jsx
+++ src/components/examples/_maps/_core/ZoomControl/index.jsx
@@ -2,15 +2,14 @@ import {
   MapContainer,
   TileLayer,
 } from 'react-leaflet'
+import { createControlComponent } from '@react-leaflet/core'
+import { Control } from 'leaflet'
 
 import "leaflet/dist/leaflet.css"
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 
 import styles from './index.module.scss'
-
-import { createControlComponent } from '@react-leaflet/core'
-import { Control } from 'leaflet'
 
 export const ZoomControl = createControlComponent(
   (props) => new Control.Zoom(props),
