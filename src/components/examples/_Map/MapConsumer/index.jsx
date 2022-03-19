f2fc5a54b3ec74de8b158b75eb4d3f5f887a2fcd
= <=>
2022-03-19 17:10:57 -0300

    feat: Add component

---
+++ src/components/examples/_Map/MapConsumer/index.jsx
@@ -0,0 +1,31 @@
+import {
+  MapContainer,
+  TileLayer,
+  MapConsumer,
+} from 'react-leaflet'
+
+import "leaflet/dist/leaflet.css"
+import "leaflet-defaulticon-compatibility"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+
+import styles from './index.module.scss'
+
+
+const Map = () => {
+  return (
+    <MapContainer className={styles.Map} center={[50.5, 30.5]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MapConsumer>
+        {(map) => {
+          console.log('map center:', map.getCenter())
+          return null
+        }}
+      </MapConsumer>
+    </MapContainer>
+  )
+}
+
+export default Map
