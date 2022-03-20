714ddae905ba095778734925555bf6322878d4b8
= <=>
2022-03-20 20:16:19 -0300

    refactor: Rename folder

--- src/components/_examples/_maps/_plugins/SearchBar/index.jsx
+++ src/components/_examples/_maps/_plugins/SearchBar/index.jsx
@@ -7,6 +7,7 @@ import {
     TileLayer,
     useMap,
 } from 'react-leaflet'
+import L from 'leaflet'
 
 import "leaflet/dist/leaflet.css"
 import "leaflet-defaulticon-compatibility"
@@ -17,6 +18,13 @@ import '/node_modules/leaflet-geosearch/dist/geosearch.css'
 import { GeoSearchControl, MapBoxProvider } from 'leaflet-geosearch';
 
 import styles from './index.module.scss'
+import 'https://cdn.rawgit.com/mapshakers/mapkeyicons/master/dist/MapkeyIcons.css'
+
+const icon = L.icon({
+  // iconSize: [50, 50],
+  // popupAnchor: [2, -20],
+  iconUrl: "/images/airplane.png"
+});
 
 const SearchField = ({apiKey}) => {
     const provider = new MapBoxProvider({
@@ -28,24 +36,25 @@ const SearchField = ({apiKey}) => {
     const searchControl = new GeoSearchControl({
         provider: provider,
         style: 'bar', // optional: bar|button  - default button
-        autoComplete: true, // optional: true|false  - default true
-        autoCompleteDelay: 250, // optional: number      - default 250
+        // autoComplete: true, // optional: true|false  - default true
+        // autoCompleteDelay: 250, // optional: number      - default 250
         showMarker: true, // optional: true|false  - default true
-        showPopup: false, // optional: true|false  - default false
+        // showPopup: false, // optional: true|false  - default false
         marker: {
           // optional: L.Marker    - default L.Icon.Default
-          icon: new L.Icon.Default(),
+          icon: L.icon.glyph({ prefix: 'octicon', glyph: 'alert' }),
+          // icon: icon,
           draggable: false,
         },
-        popupFormat: ({ query, result }) => result.label, // optional: function    - default returns result label,
-        resultFormat: ({ result }) => result.label, // optional: function    - default returns result label
-        maxMarkers: 1, // optional: number      - default 1
-        retainZoomLevel: false, // optional: true|false  - default false
-        animateZoom: true, // optional: true|false  - default true
-        autoClose: false, // optional: true|false  - default false
-        searchLabel: 'Enter address', // optional: string      - default 'Enter address'
-        keepResult: false, // optional: true|false  - default false
-        updateMap: true, // optional: true|false  - default true
+        // popupFormat: ({ query, result }) => result.label, // optional: function    - default returns result label,
+        // resultFormat: ({ result }) => result.label, // optional: function    - default returns result label
+        // maxMarkers: 1, // optional: number      - default 1
+        // retainZoomLevel: false, // optional: true|false  - default false
+        // animateZoom: true, // optional: true|false  - default true
+        // autoClose: false, // optional: true|false  - default false
+        // searchLabel: 'Enter address', // optional: string      - default 'Enter address'
+        // keepResult: false, // optional: true|false  - default false
+        // updateMap: true, // optional: true|false  - default true
     });
 
     const map = useMap();
