3196ab51fd1491750b8f257a26998c7cf40d21c9
= <=>
2022-03-19 23:53:48 -0300

    improv: Reorder component and add new component

---
+++ src/components/examples/_maps/_plugins/SearchBar/index.jsx
@@ -0,0 +1,72 @@
+/*
+    Demo: https://smeijer.github.io/leaflet-geosearch/
+*/
+import { useEffect, useState } from "react"
+import {
+    MapContainer,
+    TileLayer,
+    useMap,
+} from 'react-leaflet'
+
+import "leaflet/dist/leaflet.css"
+import "leaflet-defaulticon-compatibility"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+
+import '/node_modules/leaflet-geosearch/dist/geosearch.css'
+
+import { GeoSearchControl, MapBoxProvider } from 'leaflet-geosearch';
+
+import styles from './index.module.scss'
+
+const SearchField = ({apiKey}) => {
+    const provider = new MapBoxProvider({
+        params: {
+            access_token: apiKey,
+        },
+    });
+
+    const searchControl = new GeoSearchControl({
+        provider: provider,
+        style: 'bar', // optional: bar|button  - default button
+        autoComplete: true, // optional: true|false  - default true
+        autoCompleteDelay: 250, // optional: number      - default 250
+        showMarker: true, // optional: true|false  - default true
+        showPopup: false, // optional: true|false  - default false
+        marker: {
+          // optional: L.Marker    - default L.Icon.Default
+          icon: new L.Icon.Default(),
+          draggable: false,
+        },
+        popupFormat: ({ query, result }) => result.label, // optional: function    - default returns result label,
+        resultFormat: ({ result }) => result.label, // optional: function    - default returns result label
+        maxMarkers: 1, // optional: number      - default 1
+        retainZoomLevel: false, // optional: true|false  - default false
+        animateZoom: true, // optional: true|false  - default true
+        autoClose: false, // optional: true|false  - default false
+        searchLabel: 'Enter address', // optional: string      - default 'Enter address'
+        keepResult: false, // optional: true|false  - default false
+        updateMap: true, // optional: true|false  - default true
+    });
+
+    const map = useMap();
+    useEffect(() => {
+        map.addControl(searchControl);
+        return () => map.removeControl(searchControl);
+    }, []);
+
+    return null;
+};
+
+const Map = (props) => {
+  return (
+    <MapContainer className={styles.Map} center={[50.5, 30.5]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <SearchField apiKey={process.env.NEXT_PUBLIC_MAPBOX_KEY} />
+    </MapContainer>
+  )
+}
+
+export default Map
