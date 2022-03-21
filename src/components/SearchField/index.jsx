41220a0335cdfc6b1d74a3f4b54c974285213614
= <=>
2022-03-20 22:43:25 -0300

    feat: Add new component

---
+++ src/components/SearchField/index.jsx
@@ -0,0 +1,64 @@
+/*
+    Demo: https://smeijer.github.io/leaflet-geosearch/
+*/
+import { useEffect } from 'react'
+import { useMap } from 'react-leaflet'
+import { GeoSearchControl, MapBoxProvider } from 'leaflet-geosearch'
+
+import '/node_modules/leaflet-geosearch/dist/geosearch.css'
+
+import { api } from '../../services/api';
+
+
+export const SearchField = ({ apiKey }) => {
+    // console.log('SearchField', apiKey)
+    const provider = new MapBoxProvider({
+        params: {
+            access_token: apiKey,
+        },
+    });
+
+    const searchControl = new GeoSearchControl({
+        provider: provider,
+        style: 'bar', // optional: bar|button  - default button
+        autoComplete: true, // optional: true|false  - default true
+        autoCompleteDelay: 250, // optional: number      - default 250
+        showMarker: true, // optional: true|false  - default true
+        showPopup: false, // optional: true|false  - default false
+        marker: {
+            // optional: L.Marker    - default L.Icon.Default
+            icon: new L.Icon.Default(),
+            draggable: false,
+        },
+        popupFormat: ({ query, result }) => result.label, // optional: function    - default returns result label,
+        resultFormat: ({ result }) => {
+            return result.label
+        }, // optional: function    - default returns result label
+        maxMarkers: 1, // optional: number      - default 1
+        retainZoomLevel: false, // optional: true|false  - default false
+        animateZoom: true, // optional: true|false  - default true
+        autoClose: false, // optional: true|false  - default false
+        searchLabel: 'Enter address', // optional: string      - default 'Enter address'
+        keepResult: false, // optional: true|false  - default false
+        updateMap: true, // optional: true|false  - default true
+    });
+
+    const map = useMap();
+    useEffect(() => {
+        map.addControl(searchControl);
+        return () => map.removeControl(searchControl);
+    }, []);
+    
+    map.on('geosearch/showlocation', () => {
+        async function updateMapCenter(mapCenter) {
+            console.log('mapCenter', mapCenter)
+            const { data } = await api.patch("/map_config", {"center": mapCenter})
+            return data
+        }
+
+        const mapCenter = map.getCenter()
+        updateMapCenter([mapCenter.lat, mapCenter.lng])
+    });
+
+    return null;
+}
